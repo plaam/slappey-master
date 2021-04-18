77711636400e331137c1c3b55ca4f1576792fbe5
Dan <danilk006@mail.ru>
2021-04-18 16:25:24 +0300

    Update

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import prompts from 'prompts';
+import chalk from 'chalk';
+import { questions } from './questions';
+import { handleOption } from './handler';
+
+(async () => {
+  try {
+    const args = process.argv.slice(2);
+    if (args.length === 2) {
+      // Check Command Argument
+      const [option, data] = args;
+      await handleOption(option, data);
+    } else {
+      const { option, data } = await prompts(questions);
+      await handleOption(option, data);
+    }
+  } catch (err) {
+    console.log(chalk.red.bold(err));
+  }
+})();
